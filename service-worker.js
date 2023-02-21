4492917d0f18298864751d228077dfd4ba22252f
jesus.tugade <jesus.tugade@servicenow.com>
2023-02-21 10:22:35 -0600

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// This code executes in its own worker or thread
+self.addEventListener('install', (ev) => {
+	//service worker has been installed.
+	//Extendable Event
+	console.log('installed');
+	self.skipWaiting();
+});
+
+self.addEventListener('activate', (ev) => {
+	// when the service worker has been activated to replace an old one.
+	//Extendable Event
+	console.log('activated');
+});
+
+self.addEventListener('fetch', (ev) => {
+	// ev.request each time the webpage asks for any resource.
+	//Extendable Event
+	console.log('fetch request for', ev.request.url, 'from', ev.clientId);
+});
+
+self.addEventListener('message', (ev) => {
+	//message from web page ev.data.
+	//Extendable Event
+});
